648a82ada35bad7227be763e32cc9ab1bad97c4c
vima1013 <viktor.mazepa@NetCracker.com>
2023-11-10 16:39:27 +0200

    Add simple brocker implementation

---
+++ cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8081"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+	log.Printf("Starting brocket service on port %s", webPort)
+
+	//define http server
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
