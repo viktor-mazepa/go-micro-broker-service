9269b2851ff73cc789c75798209883b573f22005
viktor-mazepa <viktor.mazepa@gmail.com>
2023-11-26 20:18:29 +0200

    Use RPC to communicate with logging-service

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"go-micro-broker-service/event"
 	"net/http"
+	"net/rpc"
 )
 
 type RequestPayload struct {
@@ -55,7 +56,8 @@ func (app *Config) HadleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.authenticate(w, requestPayload.Auth)
 	case "log":
 		//app.logItem(w, requestPayload.Log)
-		app.logEventViaRabbit(w, requestPayload.Log)
+		//app.logEventViaRabbit(w, requestPayload.Log)
+		app.logItemViaRpc(w, requestPayload.Log)
 	case "mail":
 		app.sendMail(w, requestPayload.Mail)
 	default:
@@ -131,6 +133,37 @@ func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
 
 	app.writeJSON(w, http.StatusAccepted, payload)
 
+}
+
+type RPCPayload struct {
+	Name string
+	Data string
+}
+
+func (app *Config) logItemViaRpc(w http.ResponseWriter, l LogPayload) {
+	client, err := rpc.Dial("tcp", "logger-service:5001")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	rpcPayload := RPCPayload{
+		Name: l.Name,
+		Data: l.Data,
+	}
+
+	var result string
+	err = client.Call("RPCServer.LogInfo", rpcPayload, &result)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: result,
+	}
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
 
 func (app *Config) logEventViaRabbit(w http.ResponseWriter, l LogPayload) {
